78144737c2f268c5c50c675dd9ea35edd7c9a0ad
Jacob Thomason <jacob@thomason.xxx>
2019-05-18 13:38:18 +0800

    CS fix

--- src/ObjectBuilder/template/preDelete.php
+++ src/ObjectBuilder/template/preDelete.php
@@ -2,9 +2,7 @@ if (!$ret) {
     $this-><?= $setter ?>(time());
     $this->save($con);
 
-    <?php if ($addHooks): ?>
-        $this->postDelete($con);
-    <?php endif; ?>
+    <?php if ($addHooks): ?>$this->postDelete($con);<?php endif; ?>
 
     $con->commit();
 
